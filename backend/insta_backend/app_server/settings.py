c446e613ef3fbb823849d4d4ae51e09d117ddf56
aneesh kulkarni <askthefactorcamera@gmail.com>
2021-05-29 19:49:41 +0530

    Updated allowed hosts

--- backend/insta_backend/app_server/settings.py
+++ backend/insta_backend/app_server/settings.py
@@ -31,7 +31,9 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+'https://mighty-caverns-57560.herokuapp.com/'
+]
 
 
 # Application definition
